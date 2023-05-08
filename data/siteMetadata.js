9228c733ede7dfa3fa643ac2d2b378b5bcc62ce4
NeroAsmarr <neroasmarr@gmail.com>
2023-05-08 17:18:47 +0800

    add new

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -42,12 +42,14 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      // repo: 'NeroAsmarr/myblog',
-      // repositoryId: 'R_kgDOJf0T3A',
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      // repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+      // repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+      // category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+      // categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'NeroAsmarr/myblog',
+      repositoryId: 'R_kgDOJf0T3A',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOJf0T3M4CWVno',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
