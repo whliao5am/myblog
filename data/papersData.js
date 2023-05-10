45e3b5c27146420e8e7a32c9181c8b85d1fa1c0b
NeroAsmarr <neroasmarr@gmail.com>
2023-05-10 16:41:13 +0800

    modified:   data/papersData.js

--- data/papersData.js
+++ data/papersData.js
@@ -1,7 +1,7 @@
 const PapersData = [
   {
     title: 'Real Time Bearing Fault Diagnosis Based on Convolutional Neural Network and STM32 Microcontroller',
-    description: `My first LaTeX paper, based on the IEEE conference format, is about my classroom design. It has many flaws, so I don’t plan to publish it. As a souvenir only.`,
+    description: `My first LaTeX paper, based on the IEEE conference format, is about my classroom design. It has some minor flaws, so I don’t plan to publish it. As a souvenir only.`,
     imgSrc: '/static/images/paper1.png',
     href: 'https://doi.org/10.48550/arXiv.2304.09100',
   },
