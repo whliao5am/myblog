0cda4945f3117f2dbf656ba0513ad51e6ba859cb
NeroAsmarr <neroasmarr@gmail.com>
2023-09-01 22:12:58 +0800

    modified author layout

--- data/authors/default.md
+++ data/authors/default.md
@@ -1,7 +1,8 @@
 ---
-name: Wenhao Liao
+name: Wenhao LIAO
 avatar: /static/images/avatar.png
 occupation: 👨🏻‍💻 Master’s Student | 2022 - Present
+department: 🏛️ Shenzhen Institute for Advanced Study
 company: 🇨🇳 University of Electronic Science and Technology of China
 email: neroasmarr@gmail.com
 github: https://github.com/NeroAsmarr
@@ -10,9 +11,9 @@ zhihu: https://www.zhihu.com/people/its5am
 
 Hi! 👋
 
-This is Wenhao Liao! I am majoring in cyberspace security, more precisely in relation to Advanced Persistent Threat detection. You can contact me by [Email](mailto:neroasmarr@gmail.com).
+I am Wenhao LIAO! My major is Computer Science, more precisely in relation to Cyberspace Security and System Security. Currently, my interests lie in Forensic Analysis using audit logs.
 
-- 🐣 Little Rookie
+- 🐣 The Learner
 - 🏓️ Table Tennis
 - 🥇 The Legend of Zelda: Tears of the Kingdom
 - 🤥 Trying hard to live more healthily
--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -3,7 +3,7 @@ import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
 
 export default function AuthorLayout({ children, frontMatter }) {
-  const { name, avatar, occupation, company, email, twitter, linkedin, github, zhihu } = frontMatter
+  const { name, avatar, occupation, department, company, email, twitter, linkedin, github, zhihu } = frontMatter
 
   return (
     <>
@@ -25,6 +25,7 @@ export default function AuthorLayout({ children, frontMatter }) {
             />
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
+            <div className="text-gray-500 dark:text-gray-400">{department}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
