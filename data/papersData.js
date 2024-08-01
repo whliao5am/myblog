ed11c39b6970a07c5eb4e433a7f1b253ddc7d2ca
Wenhao LIAO <neroasmarr@gmail.com>
2024-08-01 21:24:43 +0800

    Update papersData.js

--- data/papersData.js
+++ data/papersData.js
@@ -1,7 +1,7 @@
 const PapersData = [
   {
     title: 'Real Time Bearing Fault Diagnosis Based on Convolutional Neural Network and STM32 Microcontroller',
-    description: 'My first LaTeX paper, which follows the IEEE conference format, is a classroom assignment for the course of innovation and design. Because it has some shortcomings in certain aspects, I do not intend to publish it but rather keep it as a cherished memento.',
+    description: 'My FIRST LaTeX paper, formatted according to IEEE conference standards, was a classroom assignment for a course on innovation and design. Due to some flaws, I do not plan to publish it; instead, I will keep it as a memento.',
     imgSrc: '/static/images/paper1.png',
     href: 'https://doi.org/10.48550/arXiv.2304.09100',
   },
